6c41ce58ce808c43aa57a5512a2da461838da209
Per Cederberg <cederberg@gmail.com>
2005-05-08 20:49:24 +0000

    Added initial .NET assembly information file.

---
+++ src/csharp/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+/*
+ * AssemblyInfo.cs
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307, USA.
+ *
+ * Copyright (c) 2005 Per Cederberg. All rights reserved.
+ */
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("PerCederberg.Grammatica")]
+[assembly: AssemblyDescription("Grammatica Run-Time Library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("http://grammatica.percederberg.net/")]
+[assembly: AssemblyProduct("Grammatica")]
+[assembly: AssemblyCopyright("2003-2005 Per Cederberg")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.5.0.*")]
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyFile("")]
+[assembly: AssemblyKeyName("")]
