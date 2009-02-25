6edfdc3fb70909aebbcb5a5afd5e31b4f45d0acc
Per Cederberg <cederberg@gmail.com>
2009-02-25 20:50:42 +0000

    Fixed proper escaping of Unicode characters in test code.

--- test/src/java/net/percederberg/grammatica/parser/re/TestRegExp.java
+++ test/src/java/net/percederberg/grammatica/parser/re/TestRegExp.java
@@ -43,13 +43,20 @@ public class TestRegExp extends TestCase {
      * A set of normal characters from ISO-8859-1 .
      */
     private static final String LATIN_1_CHARACTERS =
-        "ÁÀÄÂÅÉÈËÊÍÌÏÎÓÒÖÔÕÚÙÜÛİáàäâãéèëêíìïîóòöôõúùüûıÿ";
+        "\u00c1\u00c0\u00c4\u00c2\u00c5\u00c9\u00c8\u00cb\u00ca\u00cd" +
+        "\u00cc\u00cf\u00ce\u00d3\u00d2\u00d6\u00d4\u00d5\u00da\u00d9" +
+        "\u00dc\u00db\u00dd\u00e1\u00e0\u00e4\u00e2\u00e3\u00e9\u00e8" +
+        "\u00eb\u00ea\u00ed\u00ec\u00ef\u00ee\u00f3\u00f2\u00f6\u00f4" +
+        "\u00f5\u00fa\u00f9\u00fc\u00fb\u00fd\u00ff";
 
     /**
      * A set of symbol characters from ISO-8859-1 .
      */
     private static final String LATIN_1_SYMBOLS =
-        "§!#¤%&/=`'½@£~-_,:;©Ş®ªß«»µ¡¿²³¼¢";
+        "\u00a7\u0021\u0023\u00a4\u0025\u0026\u002f\u003d\u0060\u0027" +
+        "\u00bd\u0040\u00a3\u007e\u002d\u005f\u002c\u003a\u003b\u00a9" +
+        "\u00de\u00ae\u00aa\u00df\u00ab\u00bb\u00b5\u00a1\u00bf\u00b2" +
+        "\u00b3\u00bc\u00a2";
 
     /**
      * A set of digit characters.
